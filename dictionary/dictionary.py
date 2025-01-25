4571155985633f528849be1f67e46071c3377245
Dmitriy Boyko <Dmitriyboyko19@gmail.com>
2025-01-25 20:30:22 +0500

    test task added new

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -7,3 +7,11 @@ def update_inventory(dictionary, key, value):
 
 def get_stock(dictionary, key, value=0):
     return dictionary.get(key, value)
+
+
+def make_user(name_user=None, age_user=None):
+    return {"name": name_user, "age": age_user}
+
+
+def format_user(dicti):
+    return f'{dicti["name"]}, {dicti["age"]}'
--- tests/test_dictionary.py
+++ tests/test_dictionary.py
@@ -1,21 +1,43 @@
 import pytest
 
-from dictionary.dictionary import get_stock, update_inventory
+from dictionary.dictionary import (
+    format_user,
+    get_stock,
+    make_user,
+    update_inventory,
+)
 
 
 @pytest.fixture
-def dictionary_for_test():
+def inventory_test():
     return {"apple": 23, "banana": 5}
 
 
-def test_update_inventory(dictionary_for_test):
-    update_inventory(dictionary_for_test, "coffee", 10)
-    assert dictionary_for_test == {"apple": 23, "banana": 5, "coffee": 10}
-
-    update_inventory(dictionary_for_test, "coffee", 10)
-    assert dictionary_for_test == {"apple": 23, "banana": 5, "coffee": 20}
+@pytest.fixture
+def user_dict_test():
+    return {"name": "Phil", "age": 25}
 
 
-def test_get_stock(dictionary_for_test):
-    assert get_stock(dictionary_for_test, "apple") == 23
-    assert get_stock(dictionary_for_test, "monkey") == 0
+def test_update_inventory(inventory_test):
+    update_inventory(inventory_test, "coffee", 10)
+    assert inventory_test == {"apple": 23, "banana": 5, "coffee": 10}
+
+    update_inventory(inventory_test, "coffee", 10)
+    assert inventory_test == {"apple": 23, "banana": 5, "coffee": 20}
+
+
+def test_get_stock(inventory_test):
+    assert get_stock(inventory_test, "apple") == 23
+    assert get_stock(inventory_test, "monkey") == 0
+
+
+def test_make_user(user_dict_test):
+    phill = make_user("Phil", 25)
+    assert phill == user_dict_test
+
+    john = make_user()
+    assert john == {"age": None, "name": None}
+
+
+def test_format_user(user_dict_test):
+    assert format_user(user_dict_test) == "Phil, 25"
